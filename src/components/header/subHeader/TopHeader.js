77f4d7984478554d786b087afa25fa7f5bb588b4
zabiRahimi <rahimi.z1360@gmail.com>
2023-08-26 21:03:34 +0330

    updating and creating sub element for sign element

--- src/components/header/subHeader/TopHeader.js
+++ src/components/header/subHeader/TopHeader.js
@@ -1,47 +1,106 @@
 
+import { useRef } from "react";
 import { Link, Outlet } from "react-router-dom";
 
-import styleTopHeader from './topHeader.module.css';
+import './topHeader.css';
 
 const TopHeader = () => {
 
+    const sign_THe = useRef(null);
+    const iUpSign_THe = useRef(null);
+    const showSignLink_THe = useRef(null);
+
+    const showSubMenuSign = () => {
+        console.log('sign');
+        sign_THe.current.classList.toggle('bgColorSign_THe');
+        iUpSign_THe.current.classList.toggle('--displayNone');
+        showSignLink_THe.current.classList.toggle('--displayNone');
+    }
+
     return (
 
-        <div className={styleTopHeader.container_header_logo}>
+        <div className='container_header_logo'>
 
 
-            <div className={styleTopHeader.logo}>absShopdfg.ir</div>
+            <div className='logo'>absShopdfg.ir</div>
 
 
-            <div className={styleTopHeader.about_us} >
+            <div className='about_us' >
                 <Link className="--styleLessLink" to='/aboutUs'>
 
-                    <i className={`icofont-info-circle ${styleTopHeader.hover_icon}`}></i>
+                    <i className='icofont-info-circle hover_icon'></i>
 
                 </Link>
 
             </div>
 
-            <div className={styleTopHeader.contact_us}>
+            <div className='contact_us'>
                 <Link className="--styleLessLink" to='/contactUs'>
 
-                    <i className={` icofont-phone-circle ${styleTopHeader.hover_icon}`}></i>
+                    <i className='icofont-phone-circle hover_icon'></i>
 
                 </Link>
 
             </div>
 
 
-            <div className={`${styleTopHeader.sing} ${styleTopHeader.hover_icon}`}>
-                <Link className="--styleLessLink" to='/sing'>
+            <div className='sign ' ref={sign_THe}>
+                <button className="--styleLessBtn hover_icon"  onClick={showSubMenuSign}>
+                    <i className="icofont-rounded-up  iUpSign_THe --displayNone" ref={iUpSign_THe} />
 
                     <i className="icofont-ui-user"></i>
                     /
                     <i className="icofont-login"></i>
 
-                </Link>
+                </button>
+                <div className="showSignLink_THe --displayNone" ref={showSignLink_THe}>
+                    <i className="icofont-caret-up iCaretUp" />
+                    {/* <button className="--styleLessBtn btnCloseSign_THe">
+                        <i className="icofont-close" />
+                    </button> */}
+                    <Link className="--styleLessLink signLink_THe" to='signIn' >
+
+                        <i className="icofont-login iSignIn" ></i>
+                        <span className="spanSignIn"> ورود </span>
+
+                    </Link>
+
+                    <Link className="--styleLessLink signLink_THe" to='signUp' >
+
+                        <i className="icofont-ui-user iSignUp"></i>
+                        <span> ثبت نام </span>
+
+                    </Link>
+                </div>
 
             </div>
+
+            {/* <div className="containerShowSignLink_THe">
+
+                <div className="divOpacity_THe"></div>
+
+                <div className="showSignLink_THe">
+
+                     <button className="--styleLessBtn btnCloseSign_THe">
+                        <i className="icofont-close" />
+                    </button>   
+                    <Link className="--styleLessLink signLink_THe" to='signIn' >
+
+                        <i className="icofont-login iSignIn" ></i>
+                        <span className="spanSignIn"> ورود </span>
+
+                    </Link>
+
+                    <Link className="--styleLessLink signLink_THe" to='signUp' >
+
+                        <i className="icofont-ui-user iSignUp"></i>
+                        <span> ثبت نام </span>
+
+                    </Link>
+                </div>
+
+            </div> */}
+
 
         </div>
 
