f9b4dcf36bca113af6a74a09d567c7ba4d3707bf
zabiRahimi <rahimi.z1360@gmail.com>
2023-09-15 16:35:36 +0330

    name change

--- src/components/header/subHeader/verticalMenu/VerticalMenu.js
+++ src/components/header/subHeader/verticalMenu/VerticalMenu.js
@@ -2,8 +2,8 @@
 import { forwardRef, useRef } from 'react';
 import { Link } from 'react-router-dom';
 
-import './containerMenu.css'
-import SubMenuVertical from '../subMenuVertical/SubMenuVertical';
+import VerticalSubmenu from './verticalSubmenu/VerticalSubmenu';
+import './verticalMenu.css'
 
 const ContainerMenu = forwardRef(({ refBtn }, ref) => {
 
@@ -38,12 +38,12 @@ const ContainerMenu = forwardRef(({ refBtn }, ref) => {
 
     const handleHideContainer = () => {
 
-        ref.containerMenu.current.classList.remove('--width100');
-        ref.containerMenu_CME.current.classList.remove('--width100');
+        ref.verticalMenu.current.classList.remove('--width100');
+        ref.verticalMenu_VME.current.classList.remove('--width100');
 
         setTimeout(function () {
 
-            ref.containerMenu.current.classList.add('--displayNone');
+            ref.verticalMenu.current.classList.add('--displayNone');
 
         }, 500);
 
@@ -59,7 +59,7 @@ const ContainerMenu = forwardRef(({ refBtn }, ref) => {
 
     const handleShowIDown = () => {
 
-        const elements = Array.from(document.getElementsByClassName('down_CME'));
+        const elements = Array.from(document.getElementsByClassName('down_VMe'));
 
         elements.forEach((element) => {
 
@@ -72,7 +72,7 @@ const ContainerMenu = forwardRef(({ refBtn }, ref) => {
 
     const handleHideIUp = () => {
 
-        const elements = Array.from(document.getElementsByClassName('up_CME'));
+        const elements = Array.from(document.getElementsByClassName('up_VMe'));
 
         elements.forEach((element) => {
 
@@ -85,7 +85,7 @@ const ContainerMenu = forwardRef(({ refBtn }, ref) => {
 
     const handleHideSubMenu = () => {
 
-        const elements = Array.from(document.getElementsByClassName('divShowSubMenu_CME'));
+        const elements = Array.from(document.getElementsByClassName('divShowSubMenu_VMe'));
 
         elements.forEach((element) => {
 
@@ -107,63 +107,63 @@ const ContainerMenu = forwardRef(({ refBtn }, ref) => {
 
     return (
 
-        <div ref={ref.containerMenu} className='containerMain_CME --displayNone ' >
+        <div ref={ref.verticalMenu} className='containerMain_VMe --displayNone ' >
 
             {/**
                 * The bottom layer has opacity
             */}
-            <div className="divOpacity_CME" onClick={handleClose}></div>
+            <div className="divOpacity_VMe" onClick={handleClose}></div>
 
-            <div className='containerMenu_CME' ref={ref.containerMenu_CME}>
+            <div className='verticalMenu_VMe' ref={ref.verticalMenu_VMe}>
 
-                <div className='divBtnClose_CME'>
+                <div className='divBtnClose_VMe'>
 
-                    <button onClick={handleClose} className='--styleLessBtn btnClose_CME'>
+                    <button onClick={handleClose} className='--styleLessBtn btnClose_VMe'>
                         <i className='icofont-close ' />
                     </button>
 
                 </div>
 
-                <div className='cotainerItems_CME'>
+                <div className='cotainerItems_VMe'>
 
-                    <div className='divItems_CME ' >
+                    <div className='divItems_VMe ' >
 
-                        <button className='--styleLessBtn btnItem_CME' onClick={() => (handleShowSubMenu(iDownPros, iUpPros, SubMenuPros))} >
+                        <button className='--styleLessBtn btnItem_VMe' onClick={() => (handleShowSubMenu(iDownPros, iUpPros, SubMenuPros))} >
 
                             <i className='icofont-tasks-alt ' />
-                            <span>محصولات</span>
-                            <i className='icofont-rounded-down down_CME lastChild_CME' ref={iDownPros} />
-                            <i className='icofont-rounded-up up_CME lastChild_CME --displayNone' ref={iUpPros} />
+                            <span> محصولات </span>
+                            <i className='icofont-rounded-down down_VMe lastChild_VMe' ref={iDownPros} />
+                            <i className='icofont-rounded-up up_VMe lastChild_VMe --displayNone' ref={iUpPros} />
 
                         </button>
 
-                        <div className='divShowSubMenu_CME  --displayNone' ref={SubMenuPros}>
+                        <div className='divShowSubMenu_VMe  --displayNone' ref={SubMenuPros}>
 
-                            <SubMenuVertical />
+                            <VerticalSubmenu />
 
                         </div>
 
-                        <button className='--styleLessBtn btnItem_CME'  >
+                        <button className='--styleLessBtn btnItem_VMe'  >
 
                             <i className='icofont-tasks-alt ' />
                             <span>موبایل</span>
-                            <i className='icofont-rounded-down lastChild_CME' />
-                            <i className='icofont-rounded-up lastChild_CME --displayNone ' />
+                            <i className='icofont-rounded-down lastChild_VMe' />
+                            <i className='icofont-rounded-up lastChild_VMe --displayNone ' />
 
                         </button>
 
                     </div>
 
-                    <div className='divItems_CME '>
+                    <div className='divItems_VMe '>
 
-                        <button className='--styleLessBtn btnItem_CME' >
+                        <button className='--styleLessBtn btnItem_VMe' >
 
                             <i className='icofont-login ' />
                             <span>ورود</span>
 
                         </button>
 
-                        <button className='--styleLessBtn btnItem_CME' >
+                        <button className='--styleLessBtn btnItem_VMe' >
 
                             <i className='icofont-ui-user ' />
                             <span>ثبت نام</span>
@@ -172,9 +172,9 @@ const ContainerMenu = forwardRef(({ refBtn }, ref) => {
 
                     </div>
 
-                    <div className='divItems_CME '>
+                    <div className='divItems_VMe '>
 
-                        <button className='--styleLessBtn btnItem_CME' >
+                        <button className='--styleLessBtn btnItem_VMe' >
 
                             <i className='icofont-ebook ' />
                             <span>راهنمای ثبت سفارش</span>
@@ -183,37 +183,37 @@ const ContainerMenu = forwardRef(({ refBtn }, ref) => {
 
                     </div>
 
-                    <div className='divItems_CME '>
+                    <div className='divItems_VMe '>
 
-                        <Link className='--styleLessLink linkItem_CME' to='/aboutUs' onClick={handleBodyScrollShow}>
+                        <Link className='--styleLessLink linkItem_VMe' to='/aboutUs' onClick={handleBodyScrollShow}>
 
                             <i className='icofont-info-circle ' />
                             <span>درباره ما</span>
 
                         </Link>
 
-                        <Link className='--styleLessLink linkItem_CME' to='/contactUs' onClick={handleBodyScrollShow}>
+                        <Link className='--styleLessLink linkItem_VMe' to='/contactUs' onClick={handleBodyScrollShow}>
 
                             <i className='icofont-phone-circle ' />
                             <span>تماس با ما</span>
 
                         </Link>
 
-                        <Link className='--styleLessLink linkItem_CME' to='/cooperationUs' onClick={handleBodyScrollShow}>
+                        <Link className='--styleLessLink linkItem_VMe' to='/cooperationUs' onClick={handleBodyScrollShow}>
 
                             <i className='icofont-handshake-deal ' />
                             <span>همکاری با ما</span>
 
                         </Link>
 
-                        <Link className='--styleLessLink linkItem_CME' to='/criticismAndSuggestions' onClick={handleBodyScrollShow}>
+                        <Link className='--styleLessLink linkItem_VMe' to='/criticismAndSuggestions' onClick={handleBodyScrollShow}>
 
                             <i className='icofont-google-talk ' />
                             <span>انتقاد و پیشنهاد</span>
 
                         </Link>
 
-                        <Link className='--styleLessLink linkItem_CME' to='/complaint' onClick={handleBodyScrollShow}>
+                        <Link className='--styleLessLink linkItem_VMe' to='/complaint' onClick={handleBodyScrollShow}>
 
                             <i className='icofont-law-order ' />
                             <span>شکایت</span>
